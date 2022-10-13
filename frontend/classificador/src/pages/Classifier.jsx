1fd64e3baf369300b96aa6497ce3b2b410a787bd
FelipePinheiro <felipe.afp27@gmail.com>
2022-10-13 16:48:15 -0300

    first page status partial

--- frontend/classificador/src/pages/Classifier.jsx
+++ frontend/classificador/src/pages/Classifier.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ButtonGeneral from "../components/Button/ButtonGeneral";
 import "../styles.scss";
 
 const Classifier = () => {
@@ -6,14 +7,19 @@ const Classifier = () => {
     <div className="classifier">
       <div className="title">Classificador de frases</div>
       <hr />
-      <div>
-        <textarea
-          name="oi"
-          id="ola"
-          cols="30"
-          rows="10"
-          placeholder="Digite sua frase aqui..."
-        />
+      <div className="inputs">
+        <div className="inputs__text">
+          <textarea
+            name="oi"
+            id="ola"
+            cols="30"
+            rows="10"
+            placeholder="Digite sua frase aqui..."
+          />
+        </div>
+        <div className="inputs__button">
+          <ButtonGeneral text="Analisar" />
+        </div>
       </div>
     </div>
   );
