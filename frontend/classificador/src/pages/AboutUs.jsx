fd5bcc22eac787a809fd032bddfcceb10d37cf2b
FelipePinheiro <felipe.afp27@gmail.com>
2022-10-22 00:03:59 -0300

    second page and logic implementation

---
+++ frontend/classificador/src/pages/AboutUs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles.scss";
+
+const AboutUs = () => {
+  return (
+    <div className="aboutUs">
+      <div className="text">
+        <div className="title">Sobre o sistema</div>
+        <br />
+        <div>
+          <span>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of t ype
+            and scrambled it to make a type specimen book. It has survive d not
+            only five centuries, but also the leap into electronic typesettin g,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
