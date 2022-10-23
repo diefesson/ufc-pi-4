6b155608b7814b81691ee58e65b49146348b429e
diefesson <diefesson.so@gmial.com>
2022-10-23 10:50:34 -0300

    #45 - Validações de requisição para o servidor de inferencia

--- ai/inferenceserver/server.py
+++ ai/inferenceserver/server.py
@@ -1,4 +1,5 @@
 import flask
+from marshmallow import Schema, fields, ValidationError
 
 from env import THRESHOLD
 from ai import vectorizer, model
@@ -7,17 +8,41 @@ NOT_OFFENSIVE_LABEL = "NOT_OFFENSIVE"
 OFFENSIVE_LABEL = "OFFENSIVE"
 
 
+request_params_schema: Schema = Schema.from_dict(
+    {
+        "comment": fields.String(required=True),
+    }
+)()
+
+
+def infer(comment):
+    features = vectorizer.transform([comment])
+    result = model.predict_proba(features)
+    not_offensive_prob, offensive_prob = result[0]
+    classification = (
+        OFFENSIVE_LABEL if offensive_prob > THRESHOLD else NOT_OFFENSIVE_LABEL
+    )
+    return not_offensive_prob, offensive_prob, classification
+
+
+def make_error_response(error_code, error_messages):
+    return {
+        "error_code": error_code,
+        "error_messages:": error_messages,
+    }, 400
+
+
 app = flask.Flask(__name__)
 
 
 @app.get("/")
 def service():
-    comment = flask.request.args["comment"]
-    result = model.predict_proba(vectorizer.transform([comment]))
-    not_offensive_prob, offensive_prob = result[0]
-    classification = (
-        OFFENSIVE_LABEL if offensive_prob > THRESHOLD else NOT_OFFENSIVE_LABEL
-    )
+    try:
+        args = request_params_schema.load(flask.request.args)
+        comment = args["comment"]
+    except ValidationError as e:
+        return make_error_response("INVALID_REQUEST", e.messages)
+    offensive_prob, not_offensive_prob, classification = infer(comment)
     return {
         "not_offensive_prob": not_offensive_prob,
         "offensive_prob": offensive_prob,
