3b1f85e92922323637ff5cef6233effa2915c364
FelipePinheiro <felipe.afp27@gmail.com>
2022-11-12 15:49:11 -0300

    add code integration in Classifier page

--- frontend/classificador/src/pages/Classifier.jsx
+++ frontend/classificador/src/pages/Classifier.jsx
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getTextRating } from "../classifier";
 import ButtonGeneral from "../components/Button/ButtonGeneral";
 import ModalFeedback from "../components/ModalFeedback/ModalFeedback";
 import "../styles.scss";
 
 const Classifier = ({ onClick }) => {
   const [text, setText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [verified, setVerified] = useState();
+  const [rating, setRating] = useState();
 
-  const handleVerifyText = () => {
-    fetch("api", {
-      comment: text,
-    })
-      .then((data) => setVerified(data))
-      .catch((err) => console.log(err));
+  const handleVerifyText = async () => {
+    setIsLoading(true);
+    setVerified(
+      await fetch(process.env.REACT_APP_URL_API, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ body: text }),
+      }).then((response) => {
+        setIsLoading(false);
+        return response.json();
+      })
+    );
   };
 
-  console.log(verified);
+  console.log(process.env.REACT_APP_URL_API);
+
+  useEffect(() => {
+    if (isLoading === false) {
+      setRating(getTextRating(verified));
+    }
+  }, [isLoading, verified]);
 
   return (
     <div className="classifier">
@@ -34,11 +51,15 @@ const Classifier = ({ onClick }) => {
           />
         </div>
         <div className="inputs__button">
-          <ButtonGeneral onClick={handleVerifyText} text="Analisar" isLoading />
+          <ButtonGeneral
+            onClick={handleVerifyText}
+            text="Analisar"
+            isLoading={isLoading}
+          />
         </div>
       </div>
       <div className="positionFeedback">
-        <ModalFeedback isApproved />
+        {rating !== null && <ModalFeedback isApproved={rating} />}
       </div>
     </div>
   );
